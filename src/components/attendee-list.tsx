abeef62e2d0df1aa7876be97427a53c5dd0fcceb
Victor Prado <prado.victor1005@gmail.com>
2024-04-03 23:05:28 -0300

    feat: introduzindo url state nas buscas

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -21,36 +21,81 @@ interface Attende {
 }
 
 export function AttendeeList() {
-  const [search, setSearch] = useState('');
-  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  });
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  });
+
+  const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<Attende[]>([]);
 
-  useEffect(() => {
-    fetch('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees').then(response => response.json()).then(data => {
-      setAttendees(data.attendees)
-    })
-  }, []);
+  const totalPages = Math.ceil(total / 10)
 
-  const totalPages = Math.ceil(attendees.length / 10)
+  useEffect(() => {
+    const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+
+    url.searchParams.set('pageIndex', String(page - 1))
+    url.searchParams.set('query', search)
+
+    fetch(url).then(response => response.json()).then(data => {
+      setAttendees(data.attendees)
+      setTotal(data.total)
+    })
+  }, [page, search]);
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('page', String(page))
+
+    window.history.pushState({}, "", url)
+
+    setPage(page)
+  }
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('search', search)
+
+    window.history.pushState({}, "", url)
+
+    setSearch(search)
+  }
 
   function handleSearch(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value)
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
 
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   return (
@@ -59,10 +104,8 @@ export function AttendeeList() {
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
-          <input onChange={handleSearch} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" type="text" placeholder="Buscar participante..." />
+          <input onChange={handleSearch} value={search} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" type="text" placeholder="Buscar participante..." />
         </div>
-
-        {search}
       </div>
 
       <Table>
@@ -108,7 +151,7 @@ export function AttendeeList() {
         </tbody>
         <tfoot>
           <TableCell colSpan={3}>
-            Mostrando 10 de {attendees.length} items
+            Mostrando {attendees.length} de {total} items
           </TableCell>
           <TableCell colSpan={3} className="text-right">
             <div className="inline-flex items-center gap-8">
